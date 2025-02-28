9c841be01ce15b17fb9b577db96a5f0e81346fa6
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-02-28 21:49:58 +0200

    starting the 1st submissions

---
+++ TERRAFORM_LAB01/network/vpc.tf
@@ -0,0 +1,19 @@
+# ==============================================
+#
+# =========== TERRAFORM FILE START =============
+#
+# ==============================================
+#
+# Created By: Hossam Mahmoud
+# Date: 2025-02-28
+# Filename: vpc.tf
+# Description: Terraform file for the vpc
+# Version: 1.0.0
+# Copyright (c) 2025 Hossam. All rights reserved.
+#
+# ==============================================
+#
+# ====== TERRAFORM IMPLEMENTATION START ======
+#
+# ==============================================
+
