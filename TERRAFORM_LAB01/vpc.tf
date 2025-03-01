28ae4f1d0c67191c2bb514f60babe12d663a5ab7
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-03-02 01:23:19 +0200

    finished the vpc

--- TERRAFORM_LAB01/vpc.tf
+++ TERRAFORM_LAB01/vpc.tf
@@ -17,8 +17,13 @@
 #
 # ==============================================
 
-terraform {
-  required_version = ">= 1.3.0"
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
 
+  tags = {
+    Name = hosa-vpc-01
+  }
 }
+
+
 
